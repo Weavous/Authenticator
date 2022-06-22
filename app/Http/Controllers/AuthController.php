ddab5dd9d9fb228e9efc844c048312b207cd870e
Wesley Flôres <wesleyfloresterres@gmail.com>
2022-06-22 20:41:00 -0300

    Create Handling Authentication Routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DeleteUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request): JsonResponse
+    {
+        $payload = array_merge($request->validated(), [
+            'password' => Hash::make($request->password)
+        ]);
+
+        return new JsonResponse(User::create($payload), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request): JsonResponse
+    {
+        $request->user()->fill($request->validated())->save();
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DeleteUserRequest $request): JsonResponse
+    {
+        $request->user()->delete();
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+}
